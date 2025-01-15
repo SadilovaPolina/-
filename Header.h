82651304336a7de46f933405348c828cb0089242
Полина Садилова <sadilovaPon06@gmail.com>
2025-01-15 17:45:52 +0500

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,15 @@
+#define Header.h
+#include <string>
+
+using namespace std;
+
+// Функция для умножения длинного числа на целое число
+string ymnojenie(const string& num, int x);
+
+// Функция для вычисления n!
+string factorial1(int n);
+
+// Функция для вычисления ! (n!)
+string factorial2(int n);
+
+
